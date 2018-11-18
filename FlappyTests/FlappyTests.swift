0cc982878f230a4889abe2469f2f39c09651bcdb
Edward Lay <edwardlay@edwards-mini.gateway>
2018-11-18 16:40:25 +1100

    Initial Commit

---
+++ FlappyTests/FlappyTests.swift
@@ -0,0 +1,36 @@
+//
+//  FlappyTests.swift
+//  FlappyTests
+//
+//  Created by Edward Lay on 6/10/17.
+//  Copyright © 2017 Edward Lay. All rights reserved.
+//
+
+import XCTest
+@testable import Flappy
+
+class FlappyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
